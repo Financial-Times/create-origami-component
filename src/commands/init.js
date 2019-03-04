13544194d7e1a806da6ffbc2fc63901fc93489a3
Jake Champion <me@jakechampion.name>
2019-03-04 12:07:23 +0000

    Update src/commands/init.js
    Co-Authored-By: gvonkoss <gabivk@gmail.com>

--- src/commands/init.js
+++ src/commands/init.js
@@ -7,7 +7,6 @@ const Boilerplate = require('../prompts/boilerplate');
 const Build = require('../tasks/build');
 
 class Init extends Command {
-	// TODO: should we use args? (e.g. for component name instead of prompting)
 
 	async run() {
 		let component = await new Boilerplate().init();
