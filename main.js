64242f5ae1beaa87e212d500c56f54f0f29a942c
Gabrielle von Koss <gabivk@gmail.com>
2019-03-05 10:43:51 +0000

    oops ran init in the root folder

--- main.js
+++
@@ -1,7 +0,0 @@
-import bob from './src/js/bob';
-const constructAll = function () {
-	bob.init();
-	document.removeEventListener('o.DOMContentLoaded', constructAll);
-};
-document.addEventListener('o.DOMContentLoaded', constructAll);
-export default bob;
