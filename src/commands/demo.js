4af848b66cd66999604cab259b2ea1a8148151a1
Gabrielle von Koss <gabivk@gmail.com>
2019-03-06 15:24:50 +0000

    omg i got one test to pass i never thought id see the day

--- src/commands/demo.js
+++ src/commands/demo.js
@@ -1,6 +1,8 @@
-const {Command, flags} = require('@oclif/command')
+const {Command, flags} = require('@oclif/command');
 const path = require('path');
 const fs = require('fs-extra');
+
+const BuildDemos = require('../tasks/build-demos');
 
 class Demo extends Command {
   constructor(argv, config) {
@@ -11,6 +13,7 @@ class Demo extends Command {
   }
 
   async run () {
+    console.log('Building demos...')
     this.readData();
   }
 
@@ -25,19 +28,33 @@ class Demo extends Command {
       // handle error
     }
 
-    this.data.defaults = demos.demosDefaults
-    this.data.variants = this.data.defaults.variants;
-    this.data.demos = demos.demos;
+    new BuildDemos(demos);
+  //   this.data.defaults = demos.demosDefaults;
 
-    this.data.demos.forEach(demo => this.generateHTML(demo))
-  }
+  //   let browserFeatures = []
+  //   if (demos.browserFeatures) {
+  //     this.data.defaults.browserFeatures = browserFeatures.concat(demos.browserFeatures.required || []).concat(demos.browserFeatures.optional || []);
+  //   }
+  //   this.data.defaults.browserFeatures.push('default');
+    
 
-  async generateHTML (demo) {
-    // let templatePath = path.join(this.cwd, this.data.defaults.template);
-    let baseFile = require(path.join(__dirname, this.templatePath, 'base.js'));
-    let destination  = path.join('demos', 'local');
-    let demoName = demo.name + '.html';
-    fs.outputFile(path.join(this.cwd, destination, demoName), baseFile(demo), 'utf-8');
+  //   if (!this.data.defaults.variants) {
+  //     //throw Error, variants are required, there is probably a better way to do this.
+  //   }
+  //   this.data.variants = this.data.defaults.variants;
+
+  //   this.data.demos = demos.demos;
+
+
+  //   this.data.demos.forEach(demo => this.generateHTML(demo))
+  // }
+
+  // async generateHTML (demo) {
+  //   // let templatePath = path.join(this.cwd, this.data.defaults.template);
+  //   let baseFile = require(path.join(__dirname, this.templatePath, 'base.js'));
+  //   let destination  = path.join('demos', 'local');
+  //   let demoName = demo.name + '.html';
+  //   fs.outputFile(path.join(this.cwd, destination, demoName), baseFile(demo, this.data.defaults), 'utf-8');
   }
 }
 
