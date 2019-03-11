471d93bdf48c3d7acaca7235fac13b45bb586245
Gabrielle von Koss <gabivk@gmail.com>
2019-03-11 17:30:07 +0000

    caveat

--- src/tasks/config.js
+++ src/tasks/config.js
@@ -32,7 +32,9 @@ class Config {
   }
 
   setDependencies () {
-    let sandboxDependencies = ['o-forms@styles', 'o-buttons']; //change @styles when o-forms 7 is released
+    // TODO: Not sure how we'll approach this (other than changing the design?), 
+    // but we can't have two brands going at once—the form will end up reflecting the brand that the user has chosen to edit ?
+    let sandboxDependencies = ['o-forms@styles', 'o-buttons']; //TODO: change @styles when o-forms 7 is released
     let dependencies = sandboxDependencies.concat(this.config.demosDefaults.dependencies);
     this.shared.dependencies = [... new Set(dependencies)];
   }
