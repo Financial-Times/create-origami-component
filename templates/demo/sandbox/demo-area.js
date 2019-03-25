7f4545f70102d73442aca7840efb7f08de95b19e
Rowan Manning <info@rowanmanning.com>
2019-03-25 13:01:50 +0000

    Allow non-configurable demos
    This allows demos to be non-configurable. Demos with no `variants`
    property in the demo defaults will now be generated correctly, but they
    will not include the sidebar. We may want to have non-customisable demos
    in the future, and this also aids development as you can get something
    up-and-running sooner.
    
    I've moved the buttons in `demo-area.js` to avoid complex nested
    ternaries. I have no idea whether this is Reacty, but named variables
    makes things easier to keep track of 🤷‍♂️

--- templates/demo/sandbox/demo-area.js
+++ templates/demo/sandbox/demo-area.js
@@ -18,6 +18,7 @@ class DemoArea extends React.PureComponent {
     super(props);
     this.ref = React.createRef();
     this.state =  {
+      sidebarEnabled: props.sidebarEnabled,
       showHTML: false,
       HTML: null,
       buttonText: 'HTML'
@@ -41,33 +42,51 @@ class DemoArea extends React.PureComponent {
   }
 
   render() {
-    return <div className="demo-area" data-o-component="o-syntax-highlight">
-      {!this.state.showHTML ? 
-        <button 
-          className="o-buttons o-buttons--mono" 
-          onClick={() => this.props.toggleSidebar()}>
-            Customise this demo
-        </button>
-        : 
-        <button
-          className="o-buttons o-buttons--mono"
-          onClick={() => alert('technically this would select this beautiful markup')}>
-          Select Full Code Snippet
-        </button>
-      }
-      <button 
-        className="o-buttons o-buttons--mono" 
+
+    console.log(this.state.sidebarEnabled);
+
+    const sidebarToggleButton = (
+      <button
+        className="o-buttons o-buttons--mono"
+        onClick={() => this.props.toggleSidebar()}>
+          Customise this demo
+      </button>
+    );
+
+    const selectFullCodeSnippetButton = (
+      <button
+        className="o-buttons o-buttons--mono"
+        onClick={() => alert('technically this would select this beautiful markup')}>
+        Select Full Code Snippet
+      </button>
+    );
+
+    const componentHtmlToggleButton = (
+      <button
+        className="o-buttons o-buttons--mono"
         onClick={() => {
           this.props.state.sidebarVisible ? this.props.toggleSidebar() : null;
           this.toggleHTML(this.ref);
         }}>
           {this.state.buttonText}
       </button>
-      <Component 
-        component={this.props.component} 
-        ref={this.ref} 
-        state={this.props.state} 
-        markup={this.state} 
+    );
+
+    return <div className="demo-area" data-o-component="o-syntax-highlight">
+      {
+        this.state.showHTML ?
+        selectFullCodeSnippetButton : (
+          this.state.sidebarEnabled ?
+          sidebarToggleButton :
+          ''
+        )
+      }
+      {componentHtmlToggleButton}
+      <Component
+        component={this.props.component}
+        ref={this.ref}
+        state={this.props.state}
+        markup={this.state}
         demo={this.props.demo} />
     </div>
   }
