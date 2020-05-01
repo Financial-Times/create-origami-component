9240501abaf6e4f8e8c8067200904a82a385a1de
notlee <lee.moody@ft.com>
2020-05-01 17:10:03 +0100

    Simplify boilerplate demos.
    The extra markup in the pa11y demo doesn't add anything.

--- templates/demos-mustache.js
+++ templates/demos-mustache.js
@@ -1,5 +1,3 @@
 module.exports = answers => {
-	return `<div>
-	<div class="${answers.name}"${answers.javascript ? `data-o-component="${answers.name}"` : ''}></div>
-</div>`;
+	return `<div class="${answers.name}"${answers.javascript ? `data-o-component="${answers.name}"` : ''}></div>\n`;
 };
--- templates/demos-pa11y.js
+++ templates/demos-pa11y.js
@@ -1,11 +1,3 @@
 module.exports = answers => {
-	return `<div>
-	<h1>Basic Demo</h1>
-	<div class="${answers.name}"${answers.javascript ? ` data-o-component="${answers.name}"` : ''}></div>
-</div>
-<div>
-	<h1>Basic Demo:hover</h1>
-	<div class="${answers.name}"${answers.javascript ? ` data-o-component="${answers.name}"` : ''}></div>
-</div>
-`;
+	return `<div class="${answers.name}"${answers.javascript ? ` data-o-component="${answers.name}"` : ''}></div>\n`;
 };
