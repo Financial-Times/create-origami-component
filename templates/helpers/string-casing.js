fed94e3f8274be69cdb3c24ebec364381ff08cef
notlee <lee.moody@ft.com>
2020-05-01 17:21:33 +0100

    Update Sass boilerplate to match boilerplate Sass test expectation.

--- templates/helpers/string-casing.js
+++ templates/helpers/string-casing.js
@@ -1,6 +1,7 @@
 module.exports = (string) => {
   return {
     original: string,
+    lowercase: string.toLowerCase(),
     titleCase: string.split('-').map(word => word.replace(word[0], word[0].toUpperCase())).join(''),
     camelCase: string.replace(/\-+(.)/g, (match, chr) => chr.toUpperCase()),
   }
