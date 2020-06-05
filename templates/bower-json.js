2eb743fd138a0d82d2c27e80e06526b87aa1cf40
Jake Champion <me@jakechampion.name>
2020-06-05 15:11:26 +0100

    Add o-brand as a dependency if brand is selected in the prompts

--- templates/bower-json.js
+++ templates/bower-json.js
@@ -1,15 +1,18 @@
 module.exports = (answers) => {
 	return `{
-  "name": "${answers.name}",
-  "description": "${answers.description}",
-  "ignore": [
-    "node_modules",
-    "bower_components",
-    "build"
-  ],
-  "main": [
-    ${answers.scss ? `"main.scss"` : ''}${answers.scss && answers.javascript ? "," : ''}
-    ${answers.javascript ? `"main.js"` : ''}
-  ]
+	"name": "${answers.name}",
+	"description": "${answers.description}",
+	"ignore": [
+		"node_modules",
+		"bower_components",
+		"build"
+	],
+	"main": [
+		${answers.scss ? `"main.scss"` : ''}${answers.scss && answers.javascript ? "," : ''}
+		${answers.javascript ? `"main.js"` : ''}
+	],
+	"dependencies": {
+		${answers.brand ? `"o-brand": "*"` : ''}
+	}
 }`;
 };
