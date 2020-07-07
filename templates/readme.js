b7e4d80018b58803e0f04e9a2739caa1bff3ee38
notlee <lee.moody@ft.com>
2020-07-07 16:06:18 +0100

    Update README boilerplate.
    https://github.com/Financial-Times/create-origami-component/issues/107

--- templates/readme.js
+++ templates/readme.js
@@ -4,63 +4,89 @@ const { camelCase } = require('./helpers/name-formats.js');
 module.exports = answers => {
 	const name = answers.name;
 
-	return `${name} [![Circle CI](https://circleci.com/gh/Financial-Times/${name}/tree/master.svg?style=svg)](https://circleci.com/gh/Financial-Times/${name}/tree/master)[![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](#licence)
-=================
-_A short description of what this component does._
-_A table of contents to help people find things_
-- [Markup](#markup)
-- [JavaScript](#javascript)
-- [Sass](#sass)
+	return `# ${name}
+
+_A short description of what the component does._
+
+- [Markup](#markup) ${answers.scss ? `
+- [Sass](#sass)`: ``} ${answers.javascript ? `
+- [JavaScript](#javascript)`: ``}
 - [Troubleshooting](#troubleshooting)
 - [Contributing](#contributing)
 - [Migration](#migration)
 - [Contact](#contact)
 - [Licence](#licence)
-_Whatever usage instructions your component has. We've broken this down by Markup, JavaScript and Sass, but it depends how complex your component is._
+
+_Usage instructions for the component. We've broken this down by Markup, JavaScript, Sass, etc. You may add more sections as needed._
 
 ### Markup
-_Common templating can go here, especially if there is only one template, but people can always check the demos for more._
+
+_Markup documentation for the component. Including css classes, data attributes, and tags for variations of the component._
+_Demo markup is also shown in the registry. For complex markup it can be useful to describe the markup and link to the registry demos for a full example._
 _Remember to start your codeblocks with three backticks and "html" so your markup is syntax highlighted correctly._
 \`\`\`html
 <div data-o-component="${name}" class='${name}'>
 </div>
 \`\`\`
 
-### JavaScript
-_Remember to start your codeblocks with three backticks and "js" so your js is syntax highlighted correctly._
-_Though it's not practical to repeat every aspect of Origami modules convention for every component, **A LOT** of people get tripped up by modules not auto initialising, so this line is useful if you have JavaScript:_
-No code will run automatically unless you are using the Build Service.
-You must either construct an \`${name}\` object or fire the \`o.DOMContentLoaded\` event, which oComponent listens for.
+${answers.scss ? `
+### Sass
 
-#### Constructing an ${name}
+_Sass documentation. With instructions to include styles with a primary mixin, what options are available, brand support where applicable._
+_For complex components it may be helpful to document apis with SassDoc and link to the components SassDoc in the Origami Registry._
+_Remember to start your codeblocks with three backticks and "scss" so your markup is syntax highlighted correctly._
+
+Use \`@include ${camelCase(name)}()\` to include styles for all \`${name}\` features.
+
+\`\`\`scss
+@include oTable();
+\`\`\`
+`: ``}
+${answers.javascript ? `
+### JavaScript
+
+_JavaScript documentation. Including how to initialise the component, available options, and common usecases._
+_For complex components it may be helpful to document apis with JSDoc and link to the components JSDocs in the Origami Registry._
+_Remember to start your codeblocks with three backticks and "js" so your js is syntax highlighted correctly._
+
+JavaScript is initialised automatically for [Origami Build Service](https://www.ft.com/__origami/service/build/v2/) users. If your project is using a manual build process, [initialise  \`${name}\` manually](https://origami.ft.com/docs/components/initialising/).
+
+For example call the \`init\` method to initialise all \`${name}\` instances in the document:
+
 \`\`\`js
-const ${camelCase(name)} = require('${name}');
+import ${camelCase(name)} from '${name}';
 ${camelCase(name)}.init();
 \`\`\`
 
-#### Firing an oDomContentLoaded event
+Or pass an element to initialise a specific \`${name}\` instance:
+
 \`\`\`js
-require('${name}');
-document.addEventListener('DOMContentLoaded', function() {
-	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-});
+import ${camelCase(name)} from '${name}';
+const ${camelCase(name)}Element = document.getElementById('#my-${name}-element');
+${camelCase(name)}.init(${camelCase(name)}Element);
 \`\`\`
 
-### Sass
-_Remember to start your codeblocks with three backticks and "sass" so your markup is syntax highlighted correctly._
-_Though it's not practical to repeat every aspect of Origami modules convention for every component, **A LOT** of people get tripped up by silent mode, so this line (remember to change the ${name} to your component name) is useful if you have Sass:_
-As with all Origami components, ${name} has a [silent mode](http://origami.ft.com/docs/syntax/scss/#silent-styles). To use its compiled CSS (rather than using its mixins with your own Sass) set \`$${name}-is-silent : false;\` in your Sass before you import the ${name} Sass.
+[Learn more about Origami component initialisation](https://origami.ft.com/docs/components/initialising/).
+`: ``}
 
 ## Troubleshooting
-_This is a good place to put problems that come up repeatedly_
-### The thing with the thing isn't working
-Fix it by turning it off and on again
+
+_Answers to questions or problems that come up repeatedly._
+_It's likely you can delete this section for now. Remember to remove it from the table of contents._
 
 ## Contributing
-If your component is particularly complicated (image sets fall into this category) then a contributing section or even a contributing.md might be useful.
+
+_Contributing documentation for non typical components._
+_It's likely you can delete this section if your component conforms with other components._
+_Remember to update the table of contents is you remove this section._
 
 ## Migration
-_We use tables to represent our migration guides. Be sure to update it when there is a major release, and update MIGRATION.md, as well_
+
+_Guides to upgrade from one major version of a component to another._
+_This section includes a table of major versions with links to \`MIGRATION.md\`._
+_\`MIGRATION.md\` would provide upgrade instructions, but since this is a new component there is no \`MIGRATION.md\`._
+_Delete this section, including from the table of contents, until there is a second major version of this component._
+
 State | Major Version | Last Minor Release | Migration guide |
 :---: | :---: | :---: | :---:
 ✨ active | 3 | N/A | [migrate to v3](MIGRATION.md#migrating-from-v2-to-v3) |
