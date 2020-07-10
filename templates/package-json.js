a2104ab8cf2ec19c1cacdc3900a77015a0ffc469
Jake Champion <me@jakechampion.name>
2020-07-10 11:32:50 +0100

    Add the remark origami linting rules

--- templates/package-json.js
+++ templates/package-json.js
@@ -5,6 +5,7 @@ module.exports = (answers) => {
 	"devDependencies": {
 		"eslint-config-origami-component": "^2.0.1",
 		"origami-ci-tools": "^2.0.0",
+		"remark-preset-lint-origami-component": "^1.1.1",
 		"stylelint-config-origami-component": "^1.0.2"
 	}
 }`;
