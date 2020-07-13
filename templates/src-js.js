25a8be4bc72a4585c88b68b5ee859af5a954881b
Jake Champion <me@jakechampion.name>
2020-07-13 15:10:03 +0100

    Update templates/src-js.js
    Co-authored-by: Lee Moody <notlee@users.noreply.github.com>

--- templates/src-js.js
+++ templates/src-js.js
@@ -46,7 +46,7 @@ module.exports = answers => {
 	 * Initialise ${name} component/s.
 	 * @param {(HTMLElement|String)} rootElement - The root element to intialise the component in, or a CSS selector for the root element
 	 * @param {Object} [options={}] - An options object for configuring the component
-	 * @returns {Example|Example[]} The newly constructed Example components
+	 * @returns {${className}|${className}[]} The newly constructed ${className} components
 	 */
 	static init (rootElement, options) {
 		if (!rootElement) {
